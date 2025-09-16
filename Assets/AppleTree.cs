c04fb35d9c918d3e6e466139b98dda112201b494
JordanS490 <jordanstaggs67@gmail.com>
2025-09-16 16:22:29 -0500

    Add files via upload

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float changeDirChance = 0.1f;
+
+    public float appleDropDelay = 1f;
+
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
+    }
+
+    void Update()
+    {
+        //Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Change Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        //} else if (Random.value < changeDirChance)
+        //{
+           // speed *= -1;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < changeDirChance)
+        {
+            speed *= -1;
+        }
+    }
+}
